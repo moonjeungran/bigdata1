76953561c7209c788a9059a1021556e79d2a563e
smhrd <smhrd@DESKTOP-F4BC2RB>
2020-09-07 16:39:56 +0900

    - 길동이에게 작업요청

--- webmvc/src/kr/smhrd/MYutil.java
+++ webmvc/src/kr/smhrd/MYutil.java
@@ -1,14 +1,13 @@
 package kr.smhrd;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 public class MYutil {
-	// 1~10 까지의 총합을 구하는 동작
+	// 한글수정
 	public int hap() {
-		int sum=0;
-		for (int i = 1; i <=10; i++) {
-			sum+=i;
-			
-		}
-		return sum;
+		// 길동아 여기좀 수정해봐~
+		return 55;
 	}
+	
 
 }
