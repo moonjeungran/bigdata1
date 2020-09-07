a775c2081642a4fbab941b16c6ec9cb6ee9cfd60
smhrd <smhrd@DESKTOP-F4BC2RB>
2020-09-07 16:44:41 +0900

    - 수정했다.

--- webmvc/src/kr/smhrd/MYutil.java
+++ webmvc/src/kr/smhrd/MYutil.java
@@ -5,8 +5,11 @@ import com.sun.org.apache.bcel.internal.generic.RETURN;
 public class MYutil {
 	// 한글수정
 	public int hap() {
-		// 길동아 여기좀 수정해봐~
-		return 55;
+		int sum=0;
+		for (int i = 1; i <= 100; i++) {
+			sum+=i;
+		}
+		return sum;
 	}
 	
 
